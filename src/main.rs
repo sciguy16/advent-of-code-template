181697400e73cf8b1181f02c0f9f1204243ab63f
David Young <david@thedavidyoung.co.uk>
2022-11-30 22:13:43 +0000

    Template implementation of FromStr and bring in color-eyre

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,33 @@
-fn part_one(_inp: &str) -> usize {
-    todo!()
+use color_eyre::Result;
+use std::str::FromStr;
+
+struct DataType;
+
+impl FromStr for DataType {
+    type Err = color_eyre::Report;
+
+    fn from_str(_inp: &str) -> std::result::Result<Self, Self::Err> {
+        Ok(Self)
+    }
 }
 
-fn part_two(_inp: &str) -> usize {
-    todo!()
+fn part_one(_inp: &DataType) -> usize {
+    0
 }
 
-fn main() {
+fn part_two(_inp: &DataType) -> usize {
+    0
+}
+
+fn main() -> Result<()> {
+    color_eyre::install()?;
     let input = include_str!("../input.txt");
-    let data = input;
-    let ans = part_one(data);
+    let data = input.parse()?;
+    let ans = part_one(&data);
     println!("part one: {}", ans);
-    let ans = part_two(data);
+    let ans = part_two(&data);
     println!("part two: {}", ans);
+    Ok(())
 }
 
 #[cfg(test)]
@@ -23,15 +38,15 @@ mod test {
 
     #[test]
     fn test_part_1() {
-        let inp = TEST_DATA;
-        let ans = part_one(inp);
+        let inp = TEST_DATA.parse().unwrap();
+        let ans = part_one(&inp);
         assert_eq!(ans, 4);
     }
 
     #[test]
     fn test_part_2() {
-        let inp = TEST_DATA;
-        let ans = part_two(inp);
+        let inp = TEST_DATA.parse().unwrap();
+        let ans = part_two(&inp);
         assert_eq!(ans, 4);
     }
 }
